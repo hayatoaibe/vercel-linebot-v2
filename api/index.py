f08ad19da43a90123de28db7bcd2b41992797c70
hayatoaibe <22211002ha@tama.ac.jp>
2025-08-01 09:10:48 +0900

    change dir

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ WEBSITE_URL = os.getenv('VERCEL_PROJECT_PRODUCTION_URL') # (アプリ名).vercel
 LINE_ACCESS_TOKEN = os.getenv('LINE_API_TOKEN') # Vercel 環境変数 "LINE_API_TOKEN" (Enviromental Variables) にAPI トークンを設定
 LINE_API_URL = "https://api.line.me/v2/bot/message/reply"
 
-policy = open("../policy.html")
+policy = open("policy.html")
 
 app = Flask(__name__)
 
